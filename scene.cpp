02dacf2b6d363b8e299bd5fd17fad8bb9a676a15
VertexC <bob2420083992@gmail.com>
2018-03-09 01:08:03 -0800

    add soil and glm, add .gitignore

---
+++ scene.cpp
@@ -0,0 +1,87 @@
+
+#include <string.h>
+#include <iostream>
+#include "scene.h"
+
+// windowsize
+GLint windowWidth = 512;
+GLint windowHeight = 512;
+float speed = 500;
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSwapBuffers();
+}
+
+void init(void)
+{
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+void mouse(int button, int state, int x, int y)
+{
+    glutPostRedisplay();
+}
+
+void reshape(int width, int height)
+{
+    glutReshapeWindow(windowWidth, windowHeight);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 033: // Escape Key
+    case 'q':
+    case 'Q':
+        exit(EXIT_SUCCESS);
+        break;
+    }
+}
+
+void specialkey(int key, int x, int y)
+{
+    switch (key)
+    {
+    }
+}
+
+void autoRotation(int value)
+{
+    glutTimerFunc(speed, autoRotation, 0);
+}
+
+int main(int argc, char **argv)
+{
+    /*OpenGL*/
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+    glutInitContextVersion(3, 2);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInitWindowPosition(300, 300);
+    glutInitWindowSize(windowWidth, windowHeight);
+    glutCreateWindow("scene");
+    glewExperimental = GL_TRUE;
+    glewInit();
+    init();
+
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans;
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutSpecialFunc(specialkey);
+    glutTimerFunc(speed, autoRotation, 0);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+    return 0;
+}
---
+++ scene.h
@@ -0,0 +1,17 @@
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+#ifdef __APPLE__  // include Mac OS X verions of headers
+#  include <OpenGL/OpenGL.h>
+#  include <GLUT/glut.h>
+#else // non-Mac OS X operating systems
+#  include <GL/glew.h>
+#  include <GL/freeglut.h>
+#  include <GL/freeglut_ext.h>
+#endif  // __APPLE__
+#include "SOIL.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#endif
